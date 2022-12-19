98d2ec0f46e392fb12e9bdc516918429fb0e23dd
Nigel Faria <nigel.faria@gmail.com>
2022-12-19 10:26:20 +0000

    Formatting document

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -2,19 +2,19 @@
 function processTransactions(transactions) {
 
 
-    if(!validateTransactions(transactions)) {
+    if (!validateTransactions(transactions)) {
         throw new Error("Undefined collection of transactions")
     }
 
     let txnCount = new Map();
-    transactions.forEach(txn => txnCount.has(txn) ? txnCount.set(txn, txnCount.get(txn)+1): txnCount.set(txn,1) );
+    transactions.forEach(txn => txnCount.has(txn) ? txnCount.set(txn, txnCount.get(txn) + 1) : txnCount.set(txn, 1));
     sortedTxnCount = sortByAmountThenName(txnCount);
-    return [...sortedTxnCount.entries()].map( ([item, count]) => `${item} ${count}`);
+    return [...sortedTxnCount.entries()].map(([item, count]) => `${item} ${count}`);
 }
 
 function sortByAmountThenName(txnCount) {
     return new Map([...txnCount.entries()].sort(([firstItem, firstItemCount], [secondItem, secondItemCount]) =>
-     secondItemCount - firstItemCount || firstItem > secondItem || -(firstItem < secondItem) ));
+        secondItemCount - firstItemCount || firstItem > secondItem || -(firstItem < secondItem)));
 }
 
 function validateTransactions(transactions) {
